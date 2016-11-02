55194723e693472816c8ddcfba0036260e883317
RSAkidinUSA <rsardb11@vt.edu>
2016-11-01 21:48:50 -0400

    added reflectance files

---
+++ firmware/src/reflectance_public.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   reflectance_public.h
+ * Author: Ryan
+ *
+ * Created on November 1, 2016, 9:45 PM
+ */
+
+#ifndef REFLECTANCE_PUBLIC_H
+#define	REFLECTANCE_PUBLIC_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* REFLECTANCE_PUBLIC_H */
+
